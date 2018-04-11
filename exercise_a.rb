82f36f38448a09968fdad005587d586a974fb339
hannahgreen1 <hannahgreen94@icloud.com>
2018-04-11 16:40:29 +0100

    exercise mainly done

--- exercise_a.rb
+++ exercise_a.rb
@@ -1,1 +1,14 @@
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+
+p stops.push("Edinburgh Waverley")
+p stops.unshift("Glasgow Quenn st")
+p stops.insert(4, "Polmont")
+p stops(5)
+p stops.delete("Livingston")
+p stops.delete(stops[2])
+p stops.length
+
+p stops.reverse
+for stop in stops
+  p stops
+end
